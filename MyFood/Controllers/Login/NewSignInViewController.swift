e19a939f90d54ff82c43b4a93604725320745439
Ty Nguyen T <developnguyen@gmail.com>
2017-05-09 17:31:36 +0700

    deleted walkthroughs files

---
+++ MyFood/Controllers/Login/NewSignInViewController.swift
@@ -0,0 +1,52 @@
+//
+//  NewSignInViewController.swift
+//  MyFood
+//
+//  Created by Nguyen Thanh Ty on 4/29/17.
+//  Copyright © 2017 Asian Tech Co., Ltd. All rights reserved.
+//
+
+import UIKit
+
+class NewSignInViewController: UIViewController {
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    swipeDownToDismiss()
+  }
+  
+  override func viewWillAppear(_ animated: Bool) {
+    super.viewWillAppear(animated)
+    self.navigationController?.setNavigationBarHidden(true, animated: animated)
+  }
+  
+  @IBAction func closeButton(_ sender: UIButton) {
+    closeView()
+  }
+  
+  func swipeDownToDismiss() {
+    let swipeDown = UISwipeGestureRecognizer(target: self, action: #selector(swipeDownAction(sender:)))
+    swipeDown.direction = UISwipeGestureRecognizerDirection.down
+    view.addGestureRecognizer(swipeDown)
+  }
+  
+  func swipeDownAction(sender: UISwipeGestureRecognizer) {
+    closeView()
+  }
+  
+  func closeView() {
+    let transition = CATransition()
+    transition.duration = 0.5
+    transition.timingFunction = CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseInEaseOut)
+    transition.type = kCATransitionPush
+    transition.subtype = kCATransitionFromBottom
+    navigationController?.view.layer.add(transition, forKey:kCATransition)
+    self.view.removeFromSuperview()
+    self.parent?.view.backgroundColor = Color.main
+  }
+  
+  @IBAction func doneButton(_ sender: Any) {
+    closeView()
+  }
+  
+}
